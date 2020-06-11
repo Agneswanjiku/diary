9e68f7e98d79ca982479a6479032700d8c0379fb
edithntinyari <emothegi@gmail.com>
2020-06-11 16:43:08 +0300

    added title paragraph in home component.html file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,35 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EventsDataService } from '../shared/events-data.service';
+import { Events } from '../shared/events.model';
+
+@Component({
+  selector: 'app-home-input',
+  templateUrl: './home-input.component.html',
+  styleUrls: ['./home-input.component.css']
+})
+export class HomeInputComponent implements OnInit {
+  event: Events;
+  constructor(private eventsDataService: EventsDataService) { }
+
+  ngOnInit() {
+  }
+
+  onAddEvent(form: NgForm) {
+    const value = form.value;
+    if (value.content) {
+      const d = new Date();
+      this.eventsDataService.addEvent(value.title, value.content, d + '')
+        .subscribe(
+          () => {
+              this.event = {_id: '', title: form.value.title, content: form.value.content, date: d + '', favorite: false};
+              form.reset();
+          }
+        );
+    }
+  }
 }
+
